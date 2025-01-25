47c57c36391b008b63f94631cde21ade60fd8e2e
icedlee337 <lee.e.norris@gmail.com>
2025-01-24 19:27:47 -0600

    fix: interpolate 4 real, 4 real

--- onix/src/functions/interpolate.function.ts
+++ onix/src/functions/interpolate.function.ts
@@ -3,5 +3,5 @@ import { ExecutorContext } from "@nx/devkit";
 export function interpolate (context: ExecutorContext, expression: string) {
     const replacements = {workspaceRoot: context.root};
 
-    return Object.entries(replacements).reduce((_, [find, replace]) => _.replace(`${find}`, replace), expression || '');
+    return Object.entries(replacements).reduce((_, [find, replace]) => _.replace('{' + find + '}', replace), expression || '');
 }
