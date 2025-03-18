8e7b5e58c904e5246276545d0955d29d583e65a3
icedlee337 <lee.e.norris@gmail.com>
2025-03-18 01:44:33 -0500

    fix: rspackConfigFactory

--- onix/src/utils/rspack-config-factory.ts
+++ onix/src/utils/rspack-config-factory.ts
@@ -1,41 +1,42 @@
 const { composePlugins, withNx } = require('@nx/rspack');
-export type TOnixRsPackConfig = {outputPath: string, entryMain?: string};
+export type TOnixRsPackConfig = { outputPath: string, entryMain?: string };
 
-module.exports = composePlugins(withNx(), ({outputPath, entryMain = './src/main.ts'}: TOnixRsPackConfig, config: any) => {
-  return {
-    entry: {
-      main: entryMain
-    },
-    output: {
-      filename: '[name].js',
-      path: outputPath
-    },
-    target: 'node',
-    experiments: {
-      topLevelAwait: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          loader: 'builtin:swc-loader',
-          options: {
-            sourceMap: true,
-            jsc: {
-              parser: {
-                syntax: 'typescript',
-                decorators: true,
-              },
-              transform: {
-                legacyDecorator: true,
-                decoratorMetadata: true,
+export const rspackConfigFactory = ({ outputPath, entryMain = './src/main.ts' }: TOnixRsPackConfig) =>
+  composePlugins(withNx(), (config) => {
+    return {
+      entry: {
+        main: entryMain
+      },
+      output: {
+        filename: '[name].js',
+        path: outputPath
+      },
+      target: 'node',
+      experiments: {
+        topLevelAwait: true,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loader: 'builtin:swc-loader',
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  decorators: true,
+                },
+                transform: {
+                  legacyDecorator: true,
+                  decoratorMetadata: true,
+                },
               },
             },
           },
-        },
-      ],
-    },
-    ...config,
-  };
-});
+        ],
+      },
+      ...config,
+    };
+  });
