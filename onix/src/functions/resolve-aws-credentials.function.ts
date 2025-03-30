811ec2f3eabbd28825e5366b27602d7d8df5d480
icedlee337 <lee.e.norris@gmail.com>
2025-03-29 20:29:36 -0500

    fix: fromIni invocation so that profile overrides env vars

--- onix/src/functions/resolve-aws-credentials.function.ts
+++ onix/src/functions/resolve-aws-credentials.function.ts
@@ -1,9 +1,16 @@
 import { fromIni } from "@aws-sdk/credential-providers";
+import { logger } from "@nx/devkit";
 
 const AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID';
 const AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY';
 
 export function resolveAwsCredentials(profile?: string | undefined) {
+    if (profile) {
+        logger.warn(`using profile "${profile}"... ensure that ${[AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY].map(_ => `"${_.toLowerCase()}"`).join(' and ')} are lowercase in your ~/.aws/credentials file`);
+
+        return fromIni({ profile });
+    }
+
     if (
         (process.env[AWS_ACCESS_KEY_ID] && process.env[AWS_SECRET_ACCESS_KEY])
         ||
@@ -11,6 +18,4 @@ export function resolveAwsCredentials(profile?: string | undefined) {
     ) {
         return undefined;
     }
-
-    return profile ? fromIni({ profile }) : undefined;
 }
