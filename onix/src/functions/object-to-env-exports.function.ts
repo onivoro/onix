f8c49d23c4ad070025605af1c677914069c1ca29
icedlee337 <lee.e.norris@gmail.com>
2025-05-19 19:08:28 -0500

    chore: add && to exports in env literal

--- onix/src/functions/object-to-env-exports.function.ts
+++ onix/src/functions/object-to-env-exports.function.ts
@@ -1,3 +1,3 @@
-export function objectToEnvExports(env: Record<string, any>): string {
-    return Object.entries(env).reduce((_, __) => `${_} export ${__[0]}='${__[1]}'`, '');
+export function objectToEnvExports(env: Record<string, any> = {}): string {
+    return Object.entries(env).map(__ => `export ${__[0]}='${__[1]}'`).join(' && ');
 }
