fe45b8463b7e2639910259c28cff2a7c39bdc86c
icedlee337 <lee.e.norris@gmail.com>
2025-06-07 19:53:47 -0500

    chore: loading env file looks for .local also

--- onix/src/functions/load-env-file.function.ts
+++ onix/src/functions/load-env-file.function.ts
@@ -2,12 +2,20 @@ import { logger } from "@nx/devkit";
 import { existsSync } from "fs";
 import { loadEnvFile as nodeLoadEnvFile } from "process";
 
+const dotLocal = '.local';
+
 export function loadEnvFile(envFile?: string | undefined) {
     if (envFile) {
-        if(existsSync(envFile)) {
+        if (existsSync(envFile)) {
             nodeLoadEnvFile(envFile);
+
+            logger.info(`loaded environment variables from file "${envFile}"`);
+
+            if (!envFile.endsWith(dotLocal)) {
+                loadEnvFile(`${envFile}${dotLocal}`);
+            }
         } else {
-            logger.warn(`specified environment path "${envFile}" does not exist`);
+            logger.warn(`optionally define environment variables in a file named "${envFile}"`);
         }
     }
 }
