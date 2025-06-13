b4afc9539747736a8605e96afcf703b85d577a7b
icedlee337 <lee.e.norris@gmail.com>
2025-06-12 19:29:46 -0500

    chore: change env overrides to inline

--- onix/src/functions/object-to-env-exports.function.ts
+++ onix/src/functions/object-to-env-exports.function.ts
@@ -1,6 +1,6 @@
 export function objectToEnvExports(env: Record<string, any> = {}): string {
     if(Object.keys(env)?.length) {
-        return Object.entries(env).map(__ => `export ${__[0]}='${__[1]}'`).join(' && ');
+        return Object.entries(env).map(__ => `${__[0]}='${__[1]}'`).join(' ');
     }
 
     return '';
