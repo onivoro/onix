bf8f6cb21c2db418c42b385c72ab029b6edb082c
icedlee337 <lee.e.norris@gmail.com>
2025-07-29 11:51:07 -0500

    chore: add external to viteConfigFactory

--- onix/src/utils/vite-config-factory.ts
+++ onix/src/utils/vite-config-factory.ts
@@ -1,12 +1,13 @@
 import { defineConfig, LibraryFormats, UserConfig } from 'vite';
+import { ExternalOption } from 'rollup';
 import dts from 'vite-plugin-dts';
 import { parse } from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-export type TOnixViteConfig = { cacheDir: string, tsconfigPath: string, entryRoot?: string, entryFileName?: string, name?: string, outDir: string, root: string };
+export type TOnixViteConfig = { cacheDir: string, tsconfigPath: string, entryRoot?: string, entryFileName?: string, name?: string, outDir: string, root: string, external?: ExternalOption };
 
 export const viteConfigFactory = (_: TOnixViteConfig, overrides: UserConfig = {}) => {
-    const { cacheDir, tsconfigPath, entryRoot = 'src', entryFileName = 'index.ts', name, outDir, root } = _;
+    const { cacheDir, tsconfigPath, entryRoot = 'src', entryFileName = 'index.ts', name, outDir, root, external } = _;
     const baseFormats: LibraryFormats[] = ['es', 'cjs'];
     const { name: fileName } = parse(entryFileName)
     return defineConfig({
@@ -37,15 +38,15 @@ export const viteConfigFactory = (_: TOnixViteConfig, overrides: UserConfig = {}
                 // Could also be a dictionary or array of multiple entry points.
                 entry: `${entryRoot}/${entryFileName}`,
                 name,
-                fileName,                
+                fileName,
                 // Change this to the formats you want to support.
                 // Don't forget to update your package.json as well.
                 formats: name ? [...baseFormats, 'umd'] : baseFormats,
             },
             rollupOptions: {
                 // External packages that should not be bundled into your library.
-                external: [],
-            },            
+                external: external || [],
+            },
         },
         ...overrides
     });
