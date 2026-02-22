849c347c7c0b44e3dd359956686a0f5a64cb413c
icedlee337 <lee.e.norris@gmail.com>
2026-02-22 13:00:47 -0500

    chore: openapi-gen executor allows additionalProperties

--- onix/src/executors/openapi-gen/executor.ts
+++ onix/src/executors/openapi-gen/executor.ts
@@ -4,8 +4,12 @@ import { ExecutorSchema } from './schema';
 
 const DEFAULT_OPENAPI_GENERATOR_VERSION = 'v6.3.0';
 
-export default executorFactory(async ({ flavor = 'typescript-axios', version = DEFAULT_OPENAPI_GENERATOR_VERSION, openapiJsonPath, outputPath }: ExecutorSchema) => {
+export default executorFactory(async ({ flavor = 'typescript-axios', version = DEFAULT_OPENAPI_GENERATOR_VERSION, additionalProperties, openapiJsonPath, outputPath }: ExecutorSchema) => {
   execSync(`rm -rf ${outputPath}`, { stdio: 'inherit' });
   execSync(`mkdir -p ${outputPath}`, { stdio: 'inherit' });
-  execSync(`docker run --rm -v .:/local openapitools/openapi-generator-cli:${version} generate -i local/${openapiJsonPath} -g ${flavor} -o local/${outputPath}`, { stdio: 'inherit' });
+  const additionalPropertiesArg = additionalProperties
+    ? ` --additional-properties ${additionalProperties}`
+    : '';
+
+  execSync(`docker run --rm -v .:/local openapitools/openapi-generator-cli:${version} generate -i local/${openapiJsonPath} -g ${flavor} -o local/${outputPath}${additionalPropertiesArg}`, { stdio: 'inherit' });
 });
--- onix/src/executors/openapi-gen/schema.d.ts
+++ onix/src/executors/openapi-gen/schema.d.ts
@@ -1,6 +1,7 @@
 export interface ExecutorSchema {
     flavor?: string;
     version?: string;
+    additionalProperties?: string;
     openapiJsonPath: string;
     outputPath: string;
 }
