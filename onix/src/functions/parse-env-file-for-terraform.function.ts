df167df7588fc7ea8c199ccfc1e6ae06bba439f6
icedlee337 <lee.e.norris@gmail.com>
2026-03-27 00:06:36 -0500

    fix: onix/src/functions/parse-env-file-for-terraform.function.ts handles balanced quotes and "="

--- onix/src/functions/parse-env-file-for-terraform.function.ts
+++ onix/src/functions/parse-env-file-for-terraform.function.ts
@@ -3,31 +3,32 @@ import { readFileSync } from 'fs';
 const encoding = 'utf-8';
 
 export function parseEnvFileForTerraform({ file, filterRegex }: { filterRegex?: RegExp, file: string }) {
+  if (!file) {
+    return {};
+  }
 
+  try {
+    const raw: string = readFileSync(file, { encoding });
 
-    if (!file) {
-        return {};
-    }
+    const lines = raw.split('\n').filter(Boolean).map(l => l.trim()).filter(Boolean);
 
-    try {
-        const raw: string = readFileSync(file, { encoding });
+    const applicableLines = lines.filter(l => l.includes('=') && l[0] !== '#');
 
-        const lines = raw.split('\n').filter(Boolean).map(l => l.trim()).filter(Boolean);
+    const filteredLines = applicableLines.filter(_ => !filterRegex || filterRegex.test(_));
 
-        const applicableLines = lines.filter(l => l.includes('=') && l[0] !== '#');
+    const keysAndValues = filteredLines.map((_) => {
+      const idx = _.indexOf('=');
+      return [_.slice(0, idx), _.slice(idx + 1)];
+    });
 
-        const filteredLines = applicableLines.filter(_ => !filterRegex || filterRegex.test(_));
+    return keysAndValues.reduce((_, [key, value]) => {
+      _[`TF_VAR_${key}`] = value.replace(/^(['"])(.*)\1$/, '$2');
+      return _;
+    }, {} as any);
 
-        const keysAndValues = filteredLines.map(_ => _.split('='));
-
-        return keysAndValues.reduce((_, [key, value]) => {
-            _[`TF_VAR_${key}`] = value;
-            return _;
-        }, {} as any);
-
-    } catch (error) {
-        console.log(error);
-        return process.exit(1);
-    }
+  } catch (error) {
+    console.log(error);
+    return process.exit(1);
+  }
 
 }
